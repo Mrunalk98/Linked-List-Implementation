98b8125a84014aa6215e7ca2d9de3754162d07e3
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-21 23:12:32 +0530

    [mrunal] Add. Created linked list and added Add method to add data to linked list

---
+++ LinkedListImplementation/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListImplementation
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data + " inserted into linked list");
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+            }
+            Console.WriteLine("\nData present in Linked List : ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- LinkedListImplementation/Program.cs
+++ LinkedListImplementation/Program.cs
@@ -7,6 +7,11 @@ namespace LinkedListImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Implementatiion!");
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
